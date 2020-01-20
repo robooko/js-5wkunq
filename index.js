e034fbe653d29fb7badf4fbc673d5344c54384a8
robooko <robooko@hotmail.com>
2020-01-20 13:59:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {PriceLink} from 'travelodge_price_components';
+
+console.log('index');
+const priceLink = document.getElementById('pricelink');
+console.log(priceLink);
+ReactDOM.render(
+  <PriceLink
+   destination='York' 
+  start='12-18-2019' end='12-21-2019' />,
+  priceLink
+);
