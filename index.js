65d0ea6697ddd92c9d8321e9b835a52f8711baa9
robooko <robooko@hotmail.com>
2020-01-21 18:12:28 +0000

    3 files modified

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ import {PriceLink, PriceGrid} from 'travelodge_price_components';
 console.log('index');
 const priceLink = document.getElementById('pricelink');
 console.log(priceLink);
+
 ReactDOM.render(
   <PriceGrid
-   destination='York' 
+   destination='London' 
   start='12-18-2019' end='12-21-2019' />,
   priceLink
 );
+
+
+const txtLocation = document.getElementById('txtLocation');
+txtLocation.addEventListener('change', (event) => {
+
+      ReactDOM.unmountComponentAtNode(priceLink);
+
+  ReactDOM.render(
+  <PriceGrid
+   destination={event.target.value} 
+  start='12-18-2019' end='12-21-2019' />,
+  priceLink
+);
+});
