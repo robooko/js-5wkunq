db512c1b57e7adeb76f0e1306039f8e0e174c37b
robooko <robooko@hotmail.com>
2020-01-21 09:30:39 +0000

    2 files modified, 13 added, 13 deleted

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
 
-import './styles/custom-less.less';
-import {PriceLink} from 'travelodge_price_components';
+import './assets/custom-less.less';
+import {PriceLink, PriceGrid} from 'travelodge_price_components';
 
 console.log('index');
 const priceLink = document.getElementById('pricelink');
 console.log(priceLink);
 ReactDOM.render(
-  <PriceLink
+  <PriceGrid
    destination='York' 
   start='12-18-2019' end='12-21-2019' />,
   priceLink
